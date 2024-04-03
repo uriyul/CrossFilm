c2905b0a74de57e4812504fee7695724352cd24e
Uri <uri@yulevich.org>
2024-04-03 21:19:24 +0300

    Added keys to nodes

--- src/Film.tsx
+++ src/Film.tsx
@@ -6,19 +6,19 @@ interface Props {
 
 function Film({ filmNode }: Props) {
   return (
-    <>
+    <div key={filmNode.title.id}>
       <img
         width="100"
         height="148"
         src={filmNode.title.primaryImage?.url}
         alt={filmNode.title.primaryImage?.caption.plainText}
       />
-      <div>
+      <div key={filmNode.title.id + "_"}>
         {String(filmNode.title.titleText.text)} (
         {String(filmNode.title.releaseYear?.year)})
       </div>
       <hr></hr>
-    </>
+    </div>
   );
 }
 
