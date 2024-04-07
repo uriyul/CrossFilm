bb3268f4837b466e07d7e3f1981bfb54b425066e
Uri <uri@yulevich.org>
2024-04-07 18:33:02 +0300

    Fixed bug of missing movies

--- src/Films.tsx
+++ src/Films.tsx
@@ -49,12 +49,12 @@ function GetFilmsForActor(
   let url = "";
   if (male) {
     url =
-      "https://caching.graphql.imdb.com/?operationName=NameMainFilmographyPaginatedCredits&variables=%7B%22after%22%3A%22bm0wMDAwMTU4L3R0MTM3MTExMS9hY3Rvcg%3D%3D%22%2C%22id%22%3A%22" +
+      "https://caching.graphql.imdb.com/?operationName=NameMainFilmographyPaginatedCredits&variables=%7B%22after%22%3A%22bm0wMDAwMTU4L3R0MDAyMzkyNi9hY3Rvcg%3D%3D%22%2C%22id%22%3A%22" +
       actorCode +
       "%22%2C%22includeUserRating%22%3Afalse%2C%22locale%22%3A%22en-US%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22sha256Hash%22%3A%224faf04583fbf1fbc7a025e5dffc7abc3486e9a04571898a27a5a1ef59c2965f3%22%2C%22version%22%3A1%7D%7D";
   } else {
     url =
-      "https://caching.graphql.imdb.com/?operationName=NameMainFilmographyPaginatedCredits&variables=%7B%22after%22%3A%22bm0yOTMzNzU3L3R0MDQ1MTI3OS9hY3RyZXNz%22%2C%22id%22%3A%22" +
+      "https://caching.graphql.imdb.com/?operationName=NameMainFilmographyPaginatedCredits&variables=%7B%22after%22%3A%22bm0yOTMzNzU3L3R0MDAyMzkyNi9hY3RyZXNz%22%2C%22id%22%3A%22" +
       actorCode +
       "%22%2C%22includeUserRating%22%3Afalse%2C%22locale%22%3A%22en-US%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22sha256Hash%22%3A%220cf092f3616dbc56105327bf09ec9f486d5fc243a1d66eb3bf791fda117c5079%22%2C%22version%22%3A1%7D%7D";
   }
